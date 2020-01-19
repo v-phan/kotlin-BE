b95c0a9f365b6a6e4662e972507504112cfe419c
vibtra <vinh.b.tran@gmail.com>
2020-01-19 17:33:39 +0100

    fjernet khttp pga uncompatible med heroku

--- src/main/kotlin/no/boapi/test/controllers/OrderController.kt
+++ src/main/kotlin/no/boapi/test/controllers/OrderController.kt
@@ -2,8 +2,11 @@ package no.boapi.test.controllers
 
 import org.apache.http.HttpHeaders
 import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.CloseableHttpResponse
+import org.apache.http.client.methods.HttpPost
 import org.apache.http.client.methods.HttpUriRequest
 import org.apache.http.client.methods.RequestBuilder
+import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,14 +17,15 @@ class OrderController {
 
     @PostMapping
     fun purchase(): String{
-        val client: HttpClient = HttpClients.custom().build()
-        val request: HttpUriRequest = RequestBuilder.get()
-                .setUri("https://apitest.vipps.no/accesstoken/get")
-                .setHeader("client_id", "c0lLeUtJUTI4b01kRnptYWhOQXE=")
-                .setHeader("client_secret", "4fe4cd4f-7998-498c-be17-056a8d9efd58")
-                .setHeader("Ocp-Apim-Subscription-Key", "982b85d191fb48ff9c655cfbc4e162c4")
-                .build()
-        client.execute(request)
+        var client: CloseableHttpClient  = HttpClients.createDefault();
+        var httpPost: HttpPost = HttpPost("http://www.example.com");
+
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
+
+        var response: CloseableHttpResponse = client.execute(httpPost);
+        client.close();
+        println(response)
         return "Ok"
     }
 }
