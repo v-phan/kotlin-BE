b5003a680d5ea24475ecab0c7ba8b67cfe824974
vibtra <vinh.b.tran@gmail.com>
2020-01-21 19:53:23 +0100

    endret tilbake til GET

--- src/main/kotlin/no/boapi/test/controllers/OrderController.kt
+++ src/main/kotlin/no/boapi/test/controllers/OrderController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class OrderController {
 
-    @PostMapping("/token")
+    @GetMapping("/token")
     fun purchase(): String{
         var client: CloseableHttpClient  = HttpClients.createDefault();
         var httpPost: HttpPost = HttpPost("https://apitest.vipps.no");
